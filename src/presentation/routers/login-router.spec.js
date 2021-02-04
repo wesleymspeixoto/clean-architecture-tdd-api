bc738fedd14c3d2675b306f675feb61428b28e61
wesleymspeixoto <wesleymiguel@gec.inatel.br>
2021-02-04 13:31:39 -0300

    ensure LoginRouter returns 400 if passwotd is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class loginRouter{
     route(httpRequest) {
-        if(!httpRequest.body.email){
+        if(!httpRequest.body.email || !httpRequest.body.password){
             return{
                 statusCode: 400
             }
@@ -15,6 +15,20 @@ describe('Login Router', () => {
         const httpRequest = {
             body: {
                 password: "any_password"
+            }
+        }
+        const httpResponse = sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+})
+
+
+describe('Login Router', () => {
+    test('Should return 400 if no password is provided', () => {
+        const sut = new loginRouter()
+        const httpRequest = {
+            body: {
+                email: "any_email@mail.com"
             }
         }
         const httpResponse = sut.route(httpRequest)
